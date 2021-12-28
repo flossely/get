34a58114c90d66debebac30c1e013b029918c39b
Longinus <jesuscaesar@outlook.com>
2021-12-28 16:41:04 +1000

    Latest update

--- gi.php
+++ gi.php
@@ -1,8 +1,51 @@
 <?php
-$thisSystem = file_get_contents('system.info');
-$getSource = file_get_contents('get.cfg');
-if ($getSource == 'https://github.com' || $getSource == 'https://www.github.com') {
-    $srcPubRepo = 'flossely';
-} else {
-    $srcPubRepo = 'webfloss';
+
+class Package
+{
+    public function info($id, $key)
+    {
+        $pkgOpen = file_get_contents($id.'.pkg');
+        $pkgExp = explode('=|1|=', $pkgOpen);
+        $pkgHead = $pkgExp[0];
+        $pkgProp = explode('=|2|=', $pkgHead);
+        return $pkgProp;
+    }
+    
+    public function files($id)
+    {
+        $pkgOpen = file_get_contents($id.'.pkg');
+        $pkgExp = explode('=|1|=', $pkgOpen);
+        $pkgBody = $pkgExp[1];
+        return $pkgBody;
+    }
 }
+
+class System
+{
+    public function __construct($id)
+    {
+        $this->id = $id;
+    }
+    
+    public function info()
+    {
+        return $this->id;
+    }
+}
+
+class Source
+{
+    public function __construct($host)
+    {
+        $this->host = $host;
+    }
+    
+    public function info()
+    {
+        return $this->host;
+    }
+}
+
+$system = new System(file_get_contents('system.info'));
+$source = new Source(file_get_contents('get.cfg'));
+$package = new Package;
